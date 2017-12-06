7b9c3db8df28af4fc663995533deff1792aff09b
Anna Susel <kamenea@gmail.com>
2017-12-06 21:19:15 +0000

    Add class ViagogoEvent

---
+++ Viagogo/ViagogoEvent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Viagogo
+{
+    class ViagogoEvent
+    {
+        private static int PreviousId;
+        private int Id;
+        private List<Ticket> TicketsList;
+
+        public static int PreviousId1 { get => PreviousId; set => PreviousId = value; }
+        public int Id1
+        {
+            get
+            {
+                return Id;
+            }
+            set
+            {
+                Id = PreviousId1 + 1;
+                PreviousId1 = PreviousId1 + 1;
+            }
+        }
+        internal List<Ticket> TicketsList1 { get => TicketsList; set => TicketsList = value; }
+
+        public ViagogoEvent(int id1, List<Ticket> ticketsList1)
+        {
+            Id = Id1;
+            TicketsList1 = ticketsList1;
+        }
+    }
+}
