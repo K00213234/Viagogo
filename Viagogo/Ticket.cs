92f7aa997367f3fdf233e9e2b8f140b3ad91cd52
Anna Susel <kamenea@gmail.com>
2017-12-06 21:05:59 +0000

    Add class Ticket

---
+++ Viagogo/Ticket.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Viagogo
+{
+    class Ticket
+    {
+        private double Price;
+
+        public double Price1
+        {
+            get
+            {
+                return Price;
+            }
+            set
+            {
+                if (value <= 0)
+                    throw new ArgumentOutOfRangeException(
+                        $"{nameof(value)} must be bigger than 0.");
+                Price = value;
+            }
+        }
+
+        public Ticket(double price)
+        {
+            Price1 = price;
+        }
+    }
+}
