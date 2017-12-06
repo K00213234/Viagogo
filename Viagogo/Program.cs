383536e49e5765f11c53544963b4111d5deb611e
Anna Susel <kamenea@gmail.com>
2017-12-06 22:14:41 +0000

    Fixed unique Id for ViagogoEvents

--- Viagogo/Program.cs
+++ Viagogo/Program.cs
@@ -9,11 +9,24 @@ namespace Viagogo
         {
             // Testing class Ticket
             Ticket Ticket = new Ticket(20.0);
+            Ticket Ticket2 = new Ticket(25.0);
+            Ticket Ticket3 = new Ticket(30.0);
 
             List<Ticket> TicketsList = new List<Ticket>();
             TicketsList.Add(Ticket);
+            TicketsList.Add(Ticket2);
+            TicketsList.Add(Ticket3);
 
             ViagogoEvent anEvent = new ViagogoEvent(TicketsList);
+
+            foreach(var t in anEvent.TicketsList1)
+            {
+                Console.WriteLine("Ticket - {0}", t.Price1);
+            }
+
+            Console.WriteLine("Event Id: {0}", anEvent.Id1);
+
+
             Console.WriteLine("New Ticket - price: {0}", Ticket.Price1);
         }
     }
--- Viagogo/ViagogoEvent.cs
+++ Viagogo/ViagogoEvent.cs
@@ -6,28 +6,25 @@ namespace Viagogo
 {
     class ViagogoEvent
     {
-        private static int PreviousId;
-        private int Id;
+        private static int PreviousId = 0;
+        private int Id = 0;
         private List<Ticket> TicketsList;
 
         public static int PreviousId1 { get => PreviousId; set => PreviousId = value; }
-        public int Id1
-        {
-            get
-            {
-                return Id;
-            }
-            set
-            {
-                Id = PreviousId1 + 1;
-                PreviousId1 = PreviousId1 + 1;
-            }
-        }
+
         internal List<Ticket> TicketsList1 { get => TicketsList; set => TicketsList = value; }
+        public int Id1 { get => Id; set => Id = value; }
+
+        public ViagogoEvent()
+        {
+            PreviousId++;
+            this.Id = PreviousId;
+        }
 
         public ViagogoEvent(List<Ticket> ticketsList1)
         {
-            Id = Id1;
+            PreviousId++;
+            this.Id = PreviousId;
             TicketsList1 = ticketsList1;
         }
     }
