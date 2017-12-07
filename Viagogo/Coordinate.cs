70338ac441ef846310551201ea1a43ae39c083a3
Anna Susel <kamenea@gmail.com>
2017-12-07 00:51:37 +0000

    Added method CalculateDistance (Manhattan method)

--- Viagogo/Coordinate.cs
+++ Viagogo/Coordinate.cs
@@ -9,16 +9,23 @@ namespace Viagogo
         private int PointX;
         private int PointY;
         private ViagogoEvent ViagogoEvent;
+        private int Distance;
 
         public int PointX1 { get => PointX; set => PointX = value; }
         public int PointY1 { get => PointY; set => PointY = value; }
         internal ViagogoEvent ViagogoEvent1 { get => ViagogoEvent; set => ViagogoEvent = value; }
+        public int Distance1 { get => Distance; set => Distance = value; }
+
+        public Coordinate()
+        {
+        }
 
         public Coordinate(int pointX1, int pointY1, ViagogoEvent viagogoEvent)
         {
             PointX1 = pointX1;
             PointY1 = pointY1;
             ViagogoEvent1 = viagogoEvent;
+            Distance1 = 0;
         }
 
         public bool IsEmpty(int pointX, int pointY)
@@ -26,6 +33,12 @@ namespace Viagogo
             if ((PointX1 == pointX) && (PointY1 == pointY))
                 return false;
             return true;
+        }
+
+        public void CalculateDistance(int targetX, int targetY)
+        {
+           // Distance is calculated as Manhattan Distance
+           Distance1 = Math.Abs(PointX1 - targetX) - Math.Abs(PointY1 - targetY);
         }
     }
 }
